7b2a2d1df61fe70aed171dcbbf8f7915a16402a0
blurry <tamsilansari29@gmail.com>
2023-11-12 15:35:19 +0530

    fixed cloudinary bug

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,13 +1,34 @@
-'use client'
-import React from 'react'
-import { CldUploadWidget } from 'next-cloudinary';
+"use client";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
 
-const UploadPage = () => {
-  return (
-    <CldUploadWidget uploadPreset='fp1cjwze'>
-        {({open}) => <button className='btn btn-primary' onClick={()=>open()}>upload</button>}
-    </CldUploadWidget>
-  )
+interface CloudinaryResult {
+  public_id: string;
 }
 
-export default UploadPage
+const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+  return (
+    <>
+      {publicId && (
+        <CldImage src={publicId} width={270} height={180} alt="mac" />
+      )}
+      <CldUploadWidget
+        uploadPreset="uyq4d56e"
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
+  );
+};
+
+export default UploadPage;
