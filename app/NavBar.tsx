a456c1e96d3c1599c323f9f8474a8a7787e3a336
blurry <tamsilansari29@gmail.com>
2023-11-12 20:53:11 +0530

    added google auth models

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -14,7 +14,9 @@ const NavBar = () => {
       </Link>
       <Link href={"/users"}>Users</Link>
       {status === "loading" && <div>Loading...</div>}
-      {status === "authenticated" && <div>{session.user?.name}</div>}
+      {status === "authenticated" && <div>{session.user?.name}
+      <Link href={'/api/auth/signout'} className="ml-3">Sign Out</Link>
+      </div>}
       {status === "unauthenticated" && (
         <Link href={"/api/auth/signin"}>Login</Link>
       )}
---
+++ middleware.ts
@@ -0,0 +1,8 @@
+export { default } from "next-auth/middleware";
+
+export const config = {
+  //*: zero or more parameters
+  //+: one or more parameters
+  //?: zero or one parameters
+  matcher: ["/users/:id*"],
+};
