b6d941b34ce419c788b54db6e13bf54adc83e3f3
blurry <tamsilansari29@gmail.com>
2023-11-12 15:59:45 +0530

    added local state

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -20,6 +20,11 @@ const UploadPage = () => {
           const info = result.info as CloudinaryResult;
           setPublicId(info.public_id);
         }}
+        options={{
+          sources: ['local'],
+          multiple: false,
+          maxFiles: 5
+        }}
       >
         {({ open }) => (
           <button className="btn btn-primary" onClick={() => open()}>
