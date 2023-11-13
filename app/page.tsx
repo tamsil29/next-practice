a4bc5eadb94c47f2dd5f86a224288d87c140a649
blurry <tamsilansari29@gmail.com>
2023-11-13 16:30:33 +0530

    optimising images

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import ProductCard from './components/ProductCard'
-import { getServerSession } from 'next-auth'
-import { authOptions } from './api/auth/[...nextauth]/route'
+import Image from "next/image";
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import mac from "@/public/images/mac.jpg";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
   return (
-    <main>
-      <h1>Hello {session && <span>{session.user?.name}</span>}</h1>
+    <main className="relative h-screen">
+      {/* <h1>Hello {session && <span>{session.user?.name}</span>}</h1>
       <Link href="/users">Users</Link>
-      <ProductCard/>
+      <ProductCard/> */}
+      <Image
+        src={"https://bit.ly/react-cover"}
+        alt={"mac"}
+        fill
+        className="object-cover"
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+        quality={100}
+        priority
+      />
     </main>
-  )
+  );
 }
